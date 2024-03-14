2eec48e4ab9a7a97500f85965f3ae783fb16f662
santiagolara1 <santylarachica38@gmail.com>
2024-03-14 04:52:07 -0500

    actualizacion
    se hizo los cambios y se pudo llegar a un home mejor

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+
+function loader(){
+  var tl = gsap.timeline()
+
+tl.from(".loader span",{
+  x:100,
+  duration:1.2,
+  stagger:.03,
+  delay:.1
+})
+tl.to(".loader span",{
+  x:-100,
+  duration:.6,
+  opacity:0,
+  stagger:.03
+})
+tl.to('.loader',{
+  duration:.5,
+  opacity:0,
+  display:"none"
+})
+tl.from('.page1 .inner-content h1 span',{
+  y:60,
+  duration:.5,
+  delay:-.3,
+  stagger:.05
+})
+}
+
+loader();
+
+
+
 function locoScroll() {
     
     gsap.registerPlugin(ScrollTrigger);
@@ -85,42 +118,34 @@ ScrollTrigger.refresh();
 locoScroll()
 
 function cursor_efectos() {
-var page1content = document.querySelector("#page1-content")
-var cursor = document.querySelector("#cursor")
-var menu = document.querySelector(".menu-overlay")
+const cursor = document.querySelector("#cursor")
+const innerContent = document.querySelector("#page1-content")
 
-//dets ayuda a identificar cordenadas del cursor
- page1content.addEventListener("mousemove", function(dets) {
-    //  cursor.style.left = dets.x+"px"
-    //  cursor.style.top = dets.y+"px"
-    gsap.to(cursor,{
-        x:dets.x,
-        y:dets.y
-    })
- })
 
- page1content.addEventListener("mouseenter", function(dets) {
+innerContent.addEventListener("mousemove",function(dets){
+  gsap.to(cursor,{
+    x:dets.x,
+    y:dets.y,
+  })
+})
 
-    gsap.to(cursor,{
-        scale: 1,
-        opacity: 1
-    })
-
- })
-
- page1content.addEventListener("mouseleave", function(dets) {
-
-    gsap.to(cursor,{
-        scale: 0,
-        opacity: 0
-    })
-
- });
-
- 
+innerContent.addEventListener("mouseenter",function(dets){
+  gsap.to(cursor,{
+      scale:1,
+      opacity:1
+  })
+})
+innerContent.addEventListener("mouseleave",function(dets){
+  gsap.to(cursor,{
+      scale:0,
+      opacity:0
+  })
+})
 }
 
+
 cursor_efectos()
+
 
 function page2Animacion() {
     
@@ -172,6 +197,55 @@ var swiper = new Swiper(".mySwiper", {
     disableOnInteraction: true,
   },
 });
+
+function menuAnim(){
+  const menuBtn = document.querySelector('#page1 #page1-content nav #menu')
+  const menu = document.querySelector('#page1 .menu')
+  const closeBtn = document.querySelector('#page1 .menu  #close')
+  const video = document.querySelector('#page1 .menu .menu-sec .vid video')
+  const playReel = document.querySelector('#page1 .menu .menu-sec .vid .play')
+  const smallLinks = document.querySelector('#page1 .menu .small-links')
+  const Links = document.querySelectorAll('#page1 .menu .menu-sec .links h3 , #page1 .menu .menu-sec .links button')
+  const border = document.querySelector('#page1 .menu .menu-border') 
+
+
+menuBtn.addEventListener('click',() => {
+  menu.classList.add('active')
+  menu.style.transition = `height .5s ease`
+  
+  gsap.from(video,{
+    scale:.1,
+    duration:1
+  })
+  gsap.from(playReel,{
+    y:-100,
+    duration:1.1
+  })
+  gsap.from(smallLinks,{
+    x:200,
+    duration:2,
+    delay:-.4
+  })
+  gsap.from(Links,{
+    y:100,
+    duration:.7,
+    stagger:.1
+  })
+  gsap.to(border,{
+   width:95+"vw",
+   duration:1.5,
+   delay:-.1
+  })
+
+})
+closeBtn.addEventListener('click',() => {
+  menu.classList.remove('active') 
+})
+
+}
+
+menuAnim();
+
 
 
 
